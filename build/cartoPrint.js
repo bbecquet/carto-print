80009738e0f2f920d1a9cb740449bf1dbd70f080
Benjamin Becquet <benjamin.becquet@gmail.com>
2016-05-22 15:12:34 +0200

    Simplify build system

--- build/cartoPrint.js
+++ build/cartoPrint.js
@@ -1,111 +1,4 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.cartoPrint = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-
-var _d = require('d3');
-
-var _d2 = _interopRequireDefault(_d);
-
-var _topojson = require('topojson');
-
-var _topojson2 = _interopRequireDefault(_topojson);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function draw() {
-    var width = 800;
-    var height = 500;
-
-    _d2.default.select("svg").remove();
-    var svg = _d2.default.select("body").append("svg").attr({ width: width, height: height });
-
-    //clip container. #clip is a reference to a def that is added at the very last moment to the svg string
-    var svgRoot = svg.append("g").attr('clip-path', 'url(#clip)');
-
-    var zoom = _d2.default.behavior.zoom().scaleExtent([1, 8]).on("zoom", zoomed);
-
-    svg.call(zoom).call(zoom.event);
-
-    function zoomed() {
-        svgRoot.attr("transform", "translate(" + _d2.default.event.translate + ")scale(" + _d2.default.event.scale + ")");
-    }
-
-    var projection = _d2.default.geo.mercator();
-    // d3.geo.conicConformal()
-    // .scale(parseInt($('.js-scale').val()))
-    // .translate([parseInt($('.js-center-x').val()), parseInt($('.js-center-y').val())])
-    // .clipAngle(90);
-
-    /*
-    const path = d3.geo.path().pointRadius(function(d) {
-        return (d.properties && d.properties.opened) ?
-            style.assembly.radius :
-            style.assemblyConstruct.radius;
-    }).projection(projection);
-    */
-
-    var path2 = _d2.default.geo.path().pointRadius(1).projection(projection);
-
-    //ocean
-    svgRoot.append('rect').attr({
-        width: width,
-        height: height,
-        'class': 'ocean'
-    });
-
-    _d2.default.json("world-50m.json", function (error, data) {
-        if (error) return console.error(error);
-        console.log(data);
-
-        svgRoot.append('path').datum(_topojson2.default.mesh(data, data.objects.land)).attr('d', _d2.default.geo.path().projection(projection)).attr('class', 'land');
-
-        svgRoot.append('path').datum(_topojson2.default.mesh(data, data.objects.countries)).attr('d', _d2.default.geo.path().projection(projection)).attr('class', 'country-boundary');
-    });
-
-    /*
-    const dataPaths = Object.keys(data);
-    dataPaths.forEach(function(dataPath) {
-        Layers[dataPath]();
-    });
-    */
-}
-
-var Layers = {
-    land: function land() {
-        //land
-        svgRoot.append("g").attr("id", "countries").selectAll("path").data(data.land.features).enter().append("path").attr("d", path).attr("fill", function (d) {
-            if (['France', 'Germany', 'United Kingdom', 'Spain', 'Belgium', 'Italy'].indexOf(d.properties.admin) > -1) {
-                return '#F4783D';
-            }
-            return '#FFF';
-        }).attr("stroke", '#FFF').attr("stroke-width", 2).attr("stroke-opacity", 1);
-    }
-
-    /*,
-     admin: function() {
-        //admin (IDF)
-        svgRoot.append("g").attr("id", "idf").selectAll("path")
-            .data(data.admin.features, function(d) { return d.geometry.coordinates[0]; })
-            .enter()
-            .append("path")
-            .attr("d", path)
-            .attr("fill", "none")
-            .attr("stroke", style.region.outline)
-            .attr("stroke-width", style.region.outlineWidth );
-    }*/
-};
-
-var app = {
-    draw: draw
-};
-
-exports.default = app;
-module.exports = exports['default'];
-
-},{"d3":2,"topojson":3}],2:[function(require,module,exports){
 !function() {
   var d3 = {
     version: "3.5.17"
@@ -9660,7 +9553,7 @@ module.exports = exports['default'];
   });
   if (typeof define === "function" && define.amd) this.d3 = d3, define(d3); else if (typeof module === "object" && module.exports) module.exports = d3; else this.d3 = d3;
 }();
-},{}],3:[function(require,module,exports){
+},{}],2:[function(require,module,exports){
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
@@ -10206,5 +10099,126 @@ module.exports = exports['default'];
   exports.presimplify = presimplify;
 
 }));
-},{}]},{},[1])(1)
+},{}],3:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _d = require('d3');
+
+var _d2 = _interopRequireDefault(_d);
+
+var _topojson = require('topojson');
+
+var _topojson2 = _interopRequireDefault(_topojson);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function draw() {
+    var width = 800;
+    var height = 500;
+
+    _d2.default.select("svg").remove();
+
+    var svg = _d2.default.select("body").append("svg").attr({ width: width, height: height });
+
+    //clip container. #clip is a reference to a def that is added at the very last moment to the svg string
+    var svgRoot = svg.append("g").attr('clip-path', 'url(#clip)');
+
+    var zoom = _d2.default.behavior.zoom().scaleExtent([1, 8]).on("zoom", zoomed);
+
+    svg.call(zoom).call(zoom.event);
+
+    function zoomed() {
+        svgRoot.attr("transform", "translate(" + _d2.default.event.translate + ")scale(" + _d2.default.event.scale + ")");
+    }
+
+    var projection = _d2.default.geo.mercator().center([2.35, 48.85]).scale(2000);
+    // d3.geo.conicConformal()
+    // .scale(parseInt($('.js-scale').val()))
+    // .translate([parseInt($('.js-center-x').val()), parseInt($('.js-center-y').val())])
+    // .clipAngle(90);
+
+    /*
+    const path = d3.geo.path().pointRadius(function(d) {
+        return (d.properties && d.properties.opened) ?
+            style.assembly.radius :
+            style.assemblyConstruct.radius;
+    }).projection(projection);
+    */
+
+    var path2 = _d2.default.geo.path().pointRadius(1).projection(projection);
+
+    //ocean
+    svgRoot.append('rect').attr({
+        width: width,
+        height: height,
+        'class': 'ocean'
+    });
+
+    //d3.json("world-50m.json", function(error, data) {
+    _d2.default.json("regions.topojson", function (error, data) {
+        if (error) return console.error(error);
+        console.log(data);
+
+        svgRoot.append('path').datum(_topojson2.default.mesh(data, data.objects.regions)).attr('d', _d2.default.geo.path().projection(projection)).attr('class', 'land country-boundary');
+
+        svgRoot.append('path').datum(_topojson2.default.mesh(data, data.objects.regions)).attr('d', _d2.default.geo.path().projection(projection)).attr('class', 'country-boundary');
+    });
+}
+
+var layer = {
+    id: 'plop',
+    source: 'plop.json',
+    type: 'single',
+    filter: function filter(f) {
+        return true;
+    }
+};
+
+// TODO: reprendre idée avec tableau
+// const Layers = {
+//     land: function() {
+//         //land
+//         svgRoot.append("g").attr("id", "countries").selectAll("path")
+//             .data(data.land.features)
+//             .enter()
+//             .append("path")
+//             .attr("d", path)
+//             .attr("fill", function(d) {
+//                 if (['France','Germany','United Kingdom', 'Spain', 'Belgium', 'Italy'].indexOf(d.properties.admin) > -1 ) {
+//                     return '#F4783D';
+//                 }
+//                 return '#FFF';
+//             } )
+//             .attr("stroke", '#FFF')
+//             .attr("stroke-width", 2)
+//             .attr("stroke-opacity", 1);
+//     }
+//
+//     /*,
+//
+//     admin: function() {
+//         //admin (IDF)
+//         svgRoot.append("g").attr("id", "idf").selectAll("path")
+//             .data(data.admin.features, function(d) { return d.geometry.coordinates[0]; })
+//             .enter()
+//             .append("path")
+//             .attr("d", path)
+//             .attr("fill", "none")
+//             .attr("stroke", style.region.outline)
+//             .attr("stroke-width", style.region.outlineWidth );
+//     }*/
+// };
+
+var app = {
+    draw: draw
+};
+
+exports.default = app;
+module.exports = exports['default'];
+
+},{"d3":1,"topojson":2}]},{},[3])(3)
 });
